d6628153d8d1e5f06085aef472cc03afdc1ab136
ketanbj <ketanbj@gmail.com>
2013-02-22 21:09:18 +0000

    anged

--- tcpcommserver/src/roman10/tutorial/tcpcommserver/TcpServer.java
+++ tcpcommserver/src/roman10/tutorial/tcpcommserver/TcpServer.java
@@ -15,63 +15,173 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TcpServer extends Activity {
+	
+	private final static String DEBUG_TAG = "TakePictureActivity";
+	private TextView textDisplay;
+    private static final int TCP_SERVER_PORT = 4444;
+    private static int count = 0;
+    int filesize = 6022386;
+    int bytesRead;
+    ServerSocket ss;
+    Socket s;
+    
+    // camera related stuff
+    private int cameraId = 0;
+    private Camera mCamera;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         textDisplay = (TextView) this.findViewById(R.id.text1);
-        textDisplay.setText("");
-        runTcpServer();
+        runTcpServerThread();
+        textDisplay.setText("Created server");
     }
-    private TextView textDisplay;
-    private static final int TCP_SERVER_PORT = 21111;
-    private static int count = 0;
-    int filesize = 6022386;
-    int bytesRead;
+    
+    @Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		mCamera.release();
+		mCamera = null;
+		super.finish();
+	}
+    
+    @Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+
+	}
+    
+    @Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		if (mCamera != null) {
+			mCamera.release();
+			mCamera = null;
+		    }
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		if(mCamera == null)
+			mCamera = Camera.open(cameraId);
+		// Have to start preview here
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		if(mCamera != null)
+		{
+		mCamera.release();
+		mCamera = null;
+		}
+		super.onStop();
+	}
+    void runTcpServerThread()
+    {
+    	new Thread(new Runnable() {
+            public void run() {
+            	runTcpServer();// do stuff that doesn't touch the UI here
+            }
+    }).start();
+    }
+    
+    private int findFrontFacingCamera() {
+  	  int cameraId = -1;
+  	    // Search for the front facing camera
+  	    int numberOfCameras = Camera.getNumberOfCameras();
+  	    for (int i = 0; i < numberOfCameras; i++) {
+  	      CameraInfo info = new CameraInfo();
+  	      Camera.getCameraInfo(i, info);
+  	      if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+  	        Log.d(DEBUG_TAG, "Camera found");
+  	        cameraId = i;
+  	        break;
+  	      }
+  	    }
+  	    return cameraId;
+  	  }
+    
+    private void startCamera()
+    {
+    	 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+			      Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+			          .show();
+			    } else {
+			      cameraId = findFrontFacingCamera();
+			      if (cameraId < 0) {
+			        Toast.makeText(this, "No front facing camera found.",
+			            Toast.LENGTH_LONG).show();
+			      } else {
+			    	  mCamera = Camera.open(cameraId);
+			      }
+			    }
+    }
+    
+    public void showMsgonui(final String str)
+    {
+    	runOnUiThread(
+				new Runnable()
+				{	
+					public void run() 
+					{
+						Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
+					}
+				}
+				);
+    }
+    
     private void runTcpServer() {
-    	ServerSocket ss = null;
     	try {
 			ss = new ServerSocket(TCP_SERVER_PORT);
-			//get the application's resources  
-			Resources resources = getResources();  
-			//ss.setSoTimeout(10000);
+			
 			while(true){
 			//accept connections
-			Socket s = ss.accept();
-			//byte [] mybytearray  = new byte [filesize];
-			InputStream is = s.getInputStream();
-			OutputStream os = s.getOutputStream();
+			s = ss.accept();
+
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
-			
-			InputStream fs;
-			fs = resources.getAssets().open("images.jpeg");  
-			//send a raw file
-			//create a buffer that has the same size as the InputStream  
-			byte[] buffer = new byte[fs.available()];  
-			fs.read(buffer);
-            
+			         
 			//receive a message
 			String incomingMsg = in.readLine() + System.getProperty("line.separator");
-			Log.i("TcpServer", "received: " + incomingMsg);
-			textDisplay.append("received: " + incomingMsg);
-			count = count + 1;
+			//Log.i("TcpServer", "received: " + incomingMsg);
+			//textDisplay.append("received: " + incomingMsg);
+			showMsgonui("received: " + incomingMsg);
+			
+			if(incomingMsg.equals("click"));
+			{
+				startCamera();
+				mCamera.takePicture(null, null, new PictureHandler(getApplicationContext(),s));
+				showMsgonui("Got Click");
+							
+			}
+			
 			//send a message
-			//String outgoingMsg = "goodbye from port " + count + System.getProperty("line.separator");
-			//out.write(outgoingMsg);
-			//out.flush();
-			os.write(buffer,0,buffer.length);
-            os.flush();
-			//Log.i("TcpServer", "sent: " + outgoingMsg);
+			String outgoingMsg = "$@#* aarrrg ! " + System.getProperty("line.separator");
+			showMsgonui("Recieved $@#* from somebody");
+			
+			out.write(outgoingMsg);
+			out.flush();
+			
+			Log.i("TcpServer", "sent: " + outgoingMsg);
+			showMsgonui("sent: " + outgoingMsg);
 			//textDisplay.append("sent: " + outgoingMsg);
 			//SystemClock.sleep(5000);
 			s.close();
