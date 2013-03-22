0e6f613d4a3dd38c35fd4577319fabf153d3a5b1
samrat.ambadekar <samrat.ambadekar@gmail.com>
2013-03-22 17:21:27 +0000

    minor changes to list features

--- FeatureDetection/src/com/example/featuredetection/FeatureActivity.java
+++ FeatureDetection/src/com/example/featuredetection/FeatureActivity.java
@@ -6,31 +6,39 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.content.Context;
 import android.content.pm.*;
 
-public class FeatureActivity extends Activity {
+public class FeatureActivity extends Activity 
+{
 	ListView list;
+	TextView tv1;
 	ArrayAdapter<String> mListViewArrayAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		list = (ListView) findViewById(R.id.ListView);
-		list.setAdapter(mListViewArrayAdapter);
+		//list = (ListView) findViewById(R.id.ListView1);
+		tv1 = (TextView) findViewById(R.id.TV1);
+		//list.setAdapter(mListViewArrayAdapter);
 		Context context = getApplicationContext();
 	    PackageManager pm = context.getPackageManager();
 	    FeatureInfo[] featureInfoList = pm.getSystemAvailableFeatures();
-	    if(featureInfoList == null){
+	    if(featureInfoList == null)
+	    {
 	          Log.e("ERROR","No feature is available");
 	    }
-	    else{
+	    else
+	    {
 	          //send feature list to List view and Log  
-	          for (int i=0; i<featureInfoList.length; i++){
-	                 String featureName = featureInfoList[i].toString();
+	          for (int i=0; i<featureInfoList.length; i++)
+	          {
+	                 String featureName = featureInfoList[i].name;
 	                 Log.d("DEBUG", "Feature available: "+ featureName);
-	                 mListViewArrayAdapter.add(featureName);
+	                 tv1.append("\n" + featureName);
+	                 //mListViewArrayAdapter.add(featureName);
 	          }
 		}
 	}
